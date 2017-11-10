b35d3b61cb561aa9d6b03091976b0480a7e3fa26
BogdanPasterak <bogdanpasterak@gmail.com>
2017-11-10 20:47:11 +0000

    adding digital panel and create table

--- script1.js
+++ script1.js
@@ -1,5 +1,6 @@
 window.onload = function (){
   countSecounds();
+  bildTable();
 }
 function countSecounds()
 {
@@ -10,17 +11,37 @@ function countSecounds()
   var hours = now.getHours();
   if (hours>=12){ hours -= 12; apm = 'PM'; }
   if (hours==0) hours = 12;
-  if (hours<10) hours = "0"+hours;
 
   var minutes = now.getMinutes();
-  if (minutes<10) minutes = "0"+minutes;
 
   var seconds = now.getSeconds();
+
+  if (hours<10) hours = "0"+hours;
+  if (minutes<10) minutes = "0"+minutes;
   if (seconds<10) seconds = "0"+seconds;
-  /*
-*/
   target.innerHTML = '<h1>' + apm + ' - ' + hours + ' : ' + minutes
       + ' : ' + seconds + '</h1>';
 
    setTimeout("countSecounds()",1000);
+}
+function bildTable()
+{
+  var tar2 = document.getElementById('myClock2');
+  var tb = document.createElement("table");
+  //tb.setAttribute("border","0");
+  tb.setAttribute("width","100%");
+  var bo = document.createElement("tbody");
+  for (var r = 0; r < 4; r++) {
+    var row = document.createElement("tr");
+    row.setAttribute("height","7px");
+    for (var c = 0; c < 8; c++) {
+      var cell = document.createElement("td");
+      cell.style.setProperty("background-color","gray");
+
+      row.appendChild(cell);
+    }
+    bo.appendChild(row);
+  }
+  tb.appendChild(bo);
+  tar2.appendChild(tb);
 }
